92c0155be9ffe0928c2e25dd1bfbd90e81f758f6
Blunimo Digital <connect@blunimodigital.com>
2026-07-01 10:15:22 +0700

    Clean up package dependencies

--- supabase/019_rename_clothing_to_fashion.sql
+++ supabase/019_rename_clothing_to_fashion.sql
@@ -1,117 +1,49 @@
-with duplicate_pairs as (
-  select
-    clothing.id as clothing_id,
-    fashion.id as fashion_id,
-    shopping.id as shopping_id,
-    clothing.sort_order as clothing_sort_order
-  from public.categories clothing
-  join public.categories shopping
-    on shopping.id = clothing.parent_category_id
-   and shopping.name = 'Shopping'
-  join public.categories fashion
-    on fashion.user_profile_id = clothing.user_profile_id
-   and fashion.type = clothing.type
-   and fashion.name = 'Fashion'
-  where clothing.name = 'Clothing'
-)
+create temporary table clothing_fashion_category_pairs on commit drop as
+select
+  clothing.id as clothing_id,
+  fashion.id as fashion_id,
+  shopping.id as shopping_id,
+  clothing.sort_order as clothing_sort_order
+from public.categories clothing
+join public.categories shopping
+  on shopping.id = clothing.parent_category_id
+ and shopping.name = 'Shopping'
+join public.categories fashion
+  on fashion.user_profile_id = clothing.user_profile_id
+ and fashion.type = clothing.type
+ and fashion.name = 'Fashion'
+where clothing.name = 'Clothing';
+
 update public.categories fashion
 set
-  parent_category_id = duplicate_pairs.shopping_id,
-  sort_order = coalesce(nullif(fashion.sort_order, 0), duplicate_pairs.clothing_sort_order)
-from duplicate_pairs
-where fashion.id = duplicate_pairs.fashion_id;
+  parent_category_id = pairs.shopping_id,
+  sort_order = coalesce(nullif(fashion.sort_order, 0), pairs.clothing_sort_order)
+from clothing_fashion_category_pairs pairs
+where fashion.id = pairs.fashion_id;
 
-with duplicate_pairs as (
-  select
-    clothing.id as clothing_id,
-    fashion.id as fashion_id
-  from public.categories clothing
-  join public.categories shopping
-    on shopping.id = clothing.parent_category_id
-   and shopping.name = 'Shopping'
-  join public.categories fashion
-    on fashion.user_profile_id = clothing.user_profile_id
-   and fashion.type = clothing.type
-   and fashion.name = 'Fashion'
-  where clothing.name = 'Clothing'
-)
 update public.transactions
-set category_id = duplicate_pairs.fashion_id
-from duplicate_pairs
-where transactions.category_id = duplicate_pairs.clothing_id;
+set category_id = pairs.fashion_id
+from clothing_fashion_category_pairs pairs
+where transactions.category_id = pairs.clothing_id;
 
-with duplicate_pairs as (
-  select
-    clothing.id as clothing_id,
-    fashion.id as fashion_id
-  from public.categories clothing
-  join public.categories shopping
-    on shopping.id = clothing.parent_category_id
-   and shopping.name = 'Shopping'
-  join public.categories fashion
-    on fashion.user_profile_id = clothing.user_profile_id
-   and fashion.type = clothing.type
-   and fashion.name = 'Fashion'
-  where clothing.name = 'Clothing'
-)
 update public.upcoming_due
-set category_id = duplicate_pairs.fashion_id
-from duplicate_pairs
-where upcoming_due.category_id = duplicate_pairs.clothing_id;
+set category_id = pairs.fashion_id
+from clothing_fashion_category_pairs pairs
+where upcoming_due.category_id = pairs.clothing_id;
 
-with duplicate_pairs as (
-  select
-    clothing.id as clothing_id,
-    fashion.id as fashion_id
-  from public.categories clothing
-  join public.categories shopping
-    on shopping.id = clothing.parent_category_id
-   and shopping.name = 'Shopping'
-  join public.categories fashion
-    on fashion.user_profile_id = clothing.user_profile_id
-   and fashion.type = clothing.type
-   and fashion.name = 'Fashion'
-  where clothing.name = 'Clothing'
-)
 update public.imported_transactions
-set category_id = duplicate_pairs.fashion_id
-from duplicate_pairs
-where imported_transactions.category_id = duplicate_pairs.clothing_id;
+set category_id = pairs.fashion_id
+from clothing_fashion_category_pairs pairs
+where imported_transactions.category_id = pairs.clothing_id;
 
-with duplicate_pairs as (
-  select
-    clothing.id as clothing_id,
-    fashion.id as fashion_id
-  from public.categories clothing
-  join public.categories shopping
-    on shopping.id = clothing.parent_category_id
-   and shopping.name = 'Shopping'
-  join public.categories fashion
-    on fashion.user_profile_id = clothing.user_profile_id
-   and fashion.type = clothing.type
-   and fashion.name = 'Fashion'
-  where clothing.name = 'Clothing'
-)
 update public.categories
-set parent_category_id = duplicate_pairs.fashion_id
-from duplicate_pairs
-where categories.parent_category_id = duplicate_pairs.clothing_id;
+set parent_category_id = pairs.fashion_id
+from clothing_fashion_category_pairs pairs
+where categories.parent_category_id = pairs.clothing_id;
 
-with duplicate_pairs as (
-  select clothing.id as clothing_id
-  from public.categories clothing
-  join public.categories shopping
-    on shopping.id = clothing.parent_category_id
-   and shopping.name = 'Shopping'
-  join public.categories fashion
-    on fashion.user_profile_id = clothing.user_profile_id
-   and fashion.type = clothing.type
-   and fashion.name = 'Fashion'
-  where clothing.name = 'Clothing'
-)
 delete from public.categories
-using duplicate_pairs
-where categories.id = duplicate_pairs.clothing_id;
+using clothing_fashion_category_pairs pairs
+where categories.id = pairs.clothing_id;
 
 update public.categories clothing
 set name = 'Fashion'
